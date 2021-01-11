30dab8f5868192fcb299d394212cb9cf48031315
DevArMoF <77109219+DevArMoF@users.noreply.github.com>
2021-01-11 22:30:11 +0300

    Update Evelyn.lua

--- Evelyn.lua
+++ Evelyn.lua
@@ -11,7 +11,7 @@ https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
 bot_id  = token:match("(%d+)")  
 Id_Sudo = Sudo
-List_Sudos = {Id_Sudo,545906637}
+List_Sudos = {Id_Sudo,296369326}
 print("\27[34m"..[[
 
 >> Best Source in Telegram
